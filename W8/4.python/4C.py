8e8de5db231b2d5f6e8a4738bf0404bf5e5cd2ad
Sanzhar1711 <47249605+Sanzhar1711@users.noreply.github.com>
2020-03-19 16:51:21 +0600

    Delete 4C.py

--- W8/4.python/4C.py
+++
@@ -1,9 +0,0 @@
-num = int(input('n: '))
-input = input('list: ').split()
-
-if len(input) != num:
-    print
-    "ERR:", num, "vs", len(input)
-else:
-    print
-    len([x for x in input if int(x) > 0])
